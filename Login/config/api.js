9a475a2379555c8676c71ebc7fc1c8b0a301f7e4
panzaihao <pzhbupt0911@163.com>
2021-01-26 00:19:36 +0800

    删除Login文件夹，这个文件名不好

--- Login/config/api.js
+++
@@ -1,7 +0,0 @@
-const urlPath = "http://192.168.137.131:8000/login/users";
-
-module.exports = {
-  //UserAdd: urlPath + "user/add", //保存用户信息
-  AuthLogin: urlPath , //微信登录
-  //AuthUserInfo: urlPath + "user/userInfo", //微信登录
-};
